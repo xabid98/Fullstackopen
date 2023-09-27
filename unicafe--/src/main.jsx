fba678fe988e23aecb5048e8db67a84d69ae0c0e
Xabier Damboreina <xabidamboriena@gmail.com>
2023-09-27 16:02:12 +0200

    ejercicio 1_8

--- unicafe--/src/main.jsx
+++ unicafe--/src/main.jsx
@@ -1,33 +1,45 @@
 import  React,  { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import PropTypes from 'prop-types';
 
-const Statistics = (props) => {
-  <>
-<h1>statistics</h1>
-      <p>Good {props.good}</p>
-      <p>Neutral {props.neutral}</p>
-      <p>Bad {props.bad}</p>
-      <p>All {(props.good-props.bad)/(props.bad+props.good+props.neutral)}</p>
-      <p>Positive {props.good/(props.bad+props.good+props.neutral)}</p>
-      
-      </>
-      }
+const Statistics = ({good,neutral,bad,all,average,positive}) => {
+
+  
+
+
+return <>
+    <p>Good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {all}</p>
+    <p>average {average}</p>
+    <p>positive {positive} %</p>
+    
+</>
+
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  all: PropTypes.number.isRequired,
+  average: PropTypes.number.isRequired,
+  positive: PropTypes.number.isRequired,
+};
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
    const [neutral, setNeutral] = useState(0)
    const [bad, setBad] = useState(0)
+  const handleClickGood=()=> setGood(good+1)
+  const handleClickNeutral=()=> setNeutral(neutral+1)
+  const handleClickBad=()=> setBad(bad+1)
+  const all = good+neutral+bad
+  const average = ((good - bad)/all)*100
+  const positive = (good/all)*100
 
-  const handleClickGood=()=> {
-    setGood(good+1)
-  }
-  const handleClickNeutral=()=> {
-    setNeutral(neutral+1)
-  }
-  const handleClickBad=()=> {
-    setBad(bad+1)
-  }
 
   return (
     <div>
@@ -38,19 +50,14 @@ const App = () => {
         <button onClick={handleClickBad}>Bad</button>
       </div>
       <h1>statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {(good-bad)/(bad+good+neutral)}</p>
-      <p>Positive {good/(bad+good+neutral)}</p>
-      
-    </div>
+        <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
+
+      </div>
   )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Statistics/>
   </React.StrictMode>,
 )
